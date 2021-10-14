3fb4e69a3779c009b110b437e8ec6e7b13c9f607
Thomas <dahlg136@morris.umn.edu>
2021-10-14 14:28:12 -0500

    Quicksort

--- src/Group0.java
+++ src/Group0.java
@@ -25,26 +25,31 @@ public class Group0 {
 		Integer [] toSort = data.clone();
 		Integer [] toSortQuick = data.clone();
 		
-		sort(toSort, 0, toSort.length-1);
+		long start = System.currentTimeMillis();
+		sort(toSort);
+		long end = System.currentTimeMillis();
 		
+		System.out.println("It took TIM sort " + (end - start) + " milis");
 		//printArray(sorted, 100);
 		
-		toSort = data.clone();
 		
 		Thread.sleep(10); //to let other things finish before timing; adds stability of runs
 
-		long start = System.currentTimeMillis();
+		long start2 = System.currentTimeMillis();
 		
 		quicksort(toSortQuick, 0, toSort.length-1);
 		
-		long end = System.currentTimeMillis();
+		long end2 = System.currentTimeMillis();
 		
-		System.out.println(end - start);
+		System.out.println("It took our sort " + (end2 - start2) + " milis");
 		
 		writeOutResult(toSort, "expectedoutput.txt");
 		writeOutResult(toSortQuick, outFileName);
 	}
 	
+	private static void sort(Integer[] toSort) {
+		Arrays.sort(toSort, new BinaryComparator());
+	}
 	// YOUR SORTING METHOD GOES HERE. 
 	// You may call other methods and use other classes. 
 	// Note: you may change the return type of the method. 
